761c1d08170b7ca1c489fe3977da6e8643fe5f50
xiaomeng <1967604922@qq.com>
2023-08-15 17:00:44 +0800

    工程初始化 commit

---
+++ easypan/src/main/java/com/easypan/Controller/testController.java
@@ -0,0 +1,15 @@
+package com.easypan.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "hello word";
+    }
+
+
+}
